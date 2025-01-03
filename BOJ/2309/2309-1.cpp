e9ac63c78d3a9db91dca9bf04ed5097753d1f903
dlstjd825 <insung.jung0825@gmail.com>
2025-01-03 13:34:34 +0900

    2309 코드 수정

--- BOJ/2309/2309-1.cpp
+++ BOJ/2309/2309-1.cpp
@@ -3,47 +3,47 @@
 using namespace std;
 
 int main() {
-	int height[9];
+    int height[9];
 
-	for (int i = 0; i < 9; i++) {
-		cin >> height[i];
-	}
+    for (int i = 0; i < 9; i++) {
+        cin >> height[i];
+    }
 
-	for (int i = 0; i < 9; i++) {
-		for (int j = 1; j < 9 - i; j++) {
-			if (height[j] < height[j - 1]) {
-				int temp = height[j];
-				height[j] = height[j - 1];
-				height[j - 1] = temp;
-			}
-		}
-	}
+    for (int i = 0; i < 9; i++) {
+        for (int j = 1; j < 9 - i; j++) {
+            if (height[j] < height[j - 1]) {
+                int temp = height[j];
+                height[j] = height[j - 1];
+                height[j - 1] = temp;
+            }
+        }
+    }
 
-	for (int a = 0; a < 9; a++) {
-		for (int b = a + 1; b < 9; b++) {
-			for (int c = b + 1; c < 9; c++) {
-				for (int d = c + 1; d < 9; d++) {
-					for (int e = d + 1; e < 9; e++) {
-						for (int f = e + 1; f < 9; f++) {
-							for (int g = f + 1; g < 9; g++) {
-								if (height[a] + height[b] + height[c] + height[d] + height[e] + height[f] + height[g] == 100) {
-									cout << height[a] << endl;
-									cout << height[b] << endl;
-									cout << height[c] << endl;
-									cout << height[d] << endl;
-									cout << height[e] << endl;
-									cout << height[f] << endl;
-									cout << height[g] << endl;
-									return 0;
-								}
-							}
+    for (int a = 0; a < 9; a++) {
+        for (int b = a + 1; b < 9; b++) {
+            for (int c = b + 1; c < 9; c++) {
+                for (int d = c + 1; d < 9; d++) {
+                    for (int e = d + 1; e < 9; e++) {
+                        for (int f = e + 1; f < 9; f++) {
+                            for (int g = f + 1; g < 9; g++) {
+                                if (height[a] + height[b] + height[c] + height[d] + height[e] + height[f] + height[g] == 100) {
+                                    cout << height[a] << endl;
+                                    cout << height[b] << endl;
+                                    cout << height[c] << endl;
+                                    cout << height[d] << endl;
+                                    cout << height[e] << endl;
+                                    cout << height[f] << endl;
+                                    cout << height[g] << endl;
+                                    return 0;
+                                }
+                            }
 
-						}
-					}
-				}
-			}
-		}
-	}
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
--- BOJ/2309/2309-2.cpp
+++ BOJ/2309/2309-2.cpp
@@ -3,40 +3,40 @@
 using namespace std;
 
 int main() {
-	int height[9];
+    int height[9];
 
-	for (int i = 0; i < 9; i++) {
-		cin >> height[i];
-	}
+    for (int i = 0; i < 9; i++) {
+        cin >> height[i];
+    }
 
-	for (int i = 0; i < 9; i++) {
-		for (int j = 1; j < 9 - i; j++) {
-			if (height[j] < height[j - 1]) {
-				int temp = height[j];
-				height[j] = height[j - 1];
-				height[j - 1] = temp;
-			}
-		}
-	}
+    for (int i = 0; i < 9; i++) {
+        for (int j = 1; j < 9 - i; j++) {
+            if (height[j] < height[j - 1]) {
+                int temp = height[j];
+                height[j] = height[j - 1];
+                height[j - 1] = temp;
+            }
+        }
+    }
 
-	int sum = 0;
+    int sum = 0;
 
-	for (int i = 0; i < 9; i++) {
-		sum += height[i];
-	}
+    for (int i = 0; i < 9; i++) {
+        sum += height[i];
+    }
 
-	int diff = sum - 100;
+    int diff = sum - 100;
 
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			if (height[i] + height[j] == diff) {
-				for (int k = 0; k < 9; k++) {
-					if (k != i && k != j) {
-						cout << height[k] << endl;
-					}
-				}
-				return 0;
-			}
-		}
-	}
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (height[i] + height[j] == diff) {
+                for (int k = 0; k < 9; k++) {
+                    if (k != i && k != j) {
+                        cout << height[k] << endl;
+                    }
+                }
+                return 0;
+            }
+        }
+    }
 }
