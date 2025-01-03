accfb00b1a55f3e2bf688e1c2402ac458c3da04f
dlstjd825 <insung.jung0825@gmail.com>
2025-01-03 13:31:35 +0900

    10819 코드 수정[A

--- BOJ/10819/10819.cpp
+++ BOJ/10819/10819.cpp
@@ -5,28 +5,28 @@ using namespace std;
 int ary[9];
 
 int abs(int a, int b) {
-	return a > b ? a - b : b - a;
+    return a > b ? a - b : b - a;
 }
 
 int main() {
-	int n;
-	cin >> n;
-	
-	for (int i = 0; i < n; i++) {
-		cin >> ary[i];
-	}
-	 
-	sort(ary, ary + n);
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> ary[i];
+    }
+     
+    sort(ary, ary + n);
 
-	int rst = 0;
+    int rst = 0;
 
-	do {
-		int sum = 0;
-		for (int i = 0; i < n - 1; i++) {
-			sum += abs(ary[i] - ary[i + 1]);
-		}
-		rst = rst > sum ? rst : sum;
-	} while (next_permutation(ary, ary + n));
+    do {
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sum += abs(ary[i] - ary[i + 1]);
+        }
+        rst = rst > sum ? rst : sum;
+    } while (next_permutation(ary, ary + n));
 
-	cout << rst;
+    cout << rst;
 }
